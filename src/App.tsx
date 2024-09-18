e9f177a5ed0710c13c43e9c6b91f683e98aaa120
Izuagbe Samuel Enakhe <izuagbesamuel73@gmail.com>
2024-09-19 00:59:56 +0100

    Add project files and directories

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css"
+import { Counter } from "./features/counter/Counter"
+import { Quotes } from "./features/quotes/Quotes"
+import logo from "./logo.svg"
+
+const App = () => {
+    return (
+        <h1 className="underline">Login</h1>
+    )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html", "./views/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}

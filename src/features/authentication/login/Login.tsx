0d7010b5ed23a7eeb68f5b9d0987fae8a64f348f
Izuagbe Samuel Enakhe <izuagbesamuel73@gmail.com>
2024-09-23 02:03:07 +0100

    Add a proper error message

--- src/features/authentication/login/Login.tsx
+++ src/features/authentication/login/Login.tsx
@@ -1,118 +1,150 @@
-import { useState } from "react";
-import logo from "../../../imgs/logo.png";
+import { useState } from "react"
+import logo from "../../../imgs/logo.png"
 import { useLoginMutation } from "./authApiSlice"
-import { toast } from "react-toastify";
+import { toast } from "react-toastify"
 import "./Login.module.css"
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom"
 
 export const Login = () => {
+  const [username, setUserName] = useState("")
+  const [password, setPassword] = useState("")
+  const [errors, setErrors] = useState({})
+  const [login, { isError, isLoading, isSuccess }] = useLoginMutation()
 
-    const [username, setUserName] = useState("");
-    const [password, setPassword] = useState("");
-    const [errors, setErrors] = useState({});
-    const [login, { isError, isLoading, isSuccess }] = useLoginMutation()
+  const navigate = useNavigate()
 
-    const navigate = useNavigate();
+  const validate = () => {
+    let temp = {}
+    temp.userName = username !== "" ? false : true
+    temp.password = password !== "" ? false : true
+    setErrors(temp)
+    return Object.values(temp).every(x => x === "")
+  }
 
-    const validate = () => {
-        let temp = {};
-        temp.userName = username !== "" ? false : true;
-        temp.password = password !== "" ? false : true;
-        setErrors(temp);
-        return Object.values(temp).every(x => x === '');
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+    validate()
+    try {
+      await login({ username, password }).unwrap()
+    } catch (err) {
+      if (isError) {
+        toast.error(
+          `Failed to log in: ${err?.data?.detail ? err?.data?.error : "An error occurred"}`,
+        )
+      }
     }
+  }
 
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
-        validate()
-        try {
-            await login({ username, password }).unwrap()
-        } catch (err) {
-            if (isError) {
-                toast.error(`Failed to log in: ${err.data.detail}`)
-            }
-        }
-    }
+  if (isSuccess) {
+    navigate("/")
+  }
 
-    if (isSuccess) {
-        navigate("/")
-    }
+  return (
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 mt-32">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img
+          alt="Esmart Hotel Management Software"
+          src={logo}
+          className="mx-auto h-20 w-auto"
+        />
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Sign in to dashboard
+        </h2>
+      </div>
 
-    return (
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 mt-32">
-            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                <img
-                    alt="Esmart Hotel Management Software"
-                    src={logo}
-                    className="mx-auto h-20 w-auto"
-                />
-                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                    Sign in to dashboard
-                </h2>
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form
+          action="#"
+          method="POST"
+          className="space-y-6"
+          onSubmit={handleSubmit}
+        >
+          <div>
+            <label
+              htmlFor="username"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Username
+            </label>
+            <div className="mt-2">
+              <input
+                id="username"
+                name="username"
+                type="text"
+                autoComplete="username"
+                value={username}
+                onChange={e => setUserName(e.target.value)}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6 px-2"
+              />
+              {errors.userName === true ? (
+                <span className="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
+                  Enter username
+                </span>
+              ) : (
+                ""
+              )}
             </div>
+          </div>
 
-            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                <form action="#" method="POST" className="space-y-6" onSubmit={handleSubmit}>
-                    <div>
-                        <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
-                            Username
-                        </label>
-                        <div className="mt-2">
-                            <input
-                                id="username"
-                                name="username"
-                                type="text"
-                                autoComplete="username"
-                                value={username}
-                                onChange={(e) => setUserName(e.target.value)}
-                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6 px-2"
-                            />
-                            {
-                                errors.userName === true ? <span className="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
-                                    Enter username
-                                </span> : ""
-                            }
-                        </div>
-                    </div>
-
-                    <div>
-                        <div className="flex items-center justify-between">
-                            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                                Password
-                            </label>
-                        </div>
-                        <div className="mt-2">
-                            <input
-                                id="password"
-                                name="password"
-                                type="password"
-                                autoComplete="current-password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6 px-2"
-                            />
-                            {
-                                errors.password === true ? <span className="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
-                                    Enter password!
-                                </span> : ""
-                            }
-                        </div>
-                    </div>
-
-                    <div>
-                        <button
-                            type="submit"
-                            className="flex w-full justify-center rounded-md bg-[#0432aa] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                        >
-                            {isLoading ? <svg aria-hidden="true" className="inline w-5 h-5 text-gray-200 animate-spin fill-gray-200 mx-2" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
-                                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
-                            </svg> : ""}
-                            Sign in
-                        </button>
-                    </div>
-                </form>
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Password
+              </label>
             </div>
-        </div>
-    )
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6 px-2"
+              />
+              {errors.password === true ? (
+                <span className="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
+                  Enter password!
+                </span>
+              ) : (
+                ""
+              )}
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-[#0432aa] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              {isLoading ? (
+                <svg
+                  aria-hidden="true"
+                  className="inline w-5 h-5 text-gray-200 animate-spin fill-gray-200 mx-2"
+                  viewBox="0 0 100 101"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentFill"
+                  />
+                </svg>
+              ) : (
+                ""
+              )}
+              Sign in
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }

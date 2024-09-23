b80f706198fe2c044ea1334ea5b180be2697fc90
Izuagbe Samuel Enakhe <izuagbesamuel73@gmail.com>
2024-09-23 15:30:54 +0100

    Edit login form

--- src/features/authentication/login/Login.tsx
+++ src/features/authentication/login/Login.tsx
@@ -40,7 +40,7 @@ export const Login = () => {
   }
 
   return (
-    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 mt-32">
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <img
           alt="Esmart Hotel Management Software"

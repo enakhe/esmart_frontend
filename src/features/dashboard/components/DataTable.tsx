ce0307252362c0541846c51f60f277f7857da2be
Enakhe <izuagbesam@gmail.com>
2024-10-02 15:30:22 +0100

    Fixed the nullable entry]

--- src/features/dashboard/components/DataTable.tsx
+++ src/features/dashboard/components/DataTable.tsx
@@ -5,14 +5,18 @@ import { useNavigate } from "react-router-dom"
 const DataTable = (data: { data: any[] }) => {
   const [deleteHotel] = useDeleteHotelMutation()
 
-  const navigate = useNavigate();
+  const navigate = useNavigate()
   const onDeleteHotel = async (id: string) => {
     try {
-      deleteHotel(id);
-      navigate("/dashboard/hotel");
+      deleteHotel(id)
+      navigate("/dashboard/hotel")
     } catch (err) {
       toast.error(err)
     }
+  }
+
+  if (data.data !== null) {
+    navigate("")
   }
 
   return (
@@ -80,51 +84,56 @@ const DataTable = (data: { data: any[] }) => {
           </tr>
         </thead>
         <tbody>
-          {data.data.map((item, index) => (
-            <tr
-              key={index}
-              className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
-            >
-              <td className="w-4 p-4">
-                <div className="flex items-center">
-                  <input
-                    id="checkbox-table-search-1"
-                    type="checkbox"
-                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                  />
-                  <label htmlFor="checkbox-table-search-1" className="sr-only">
-                    checkbox
-                  </label>
-                </div>
-              </td>
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                {item.Name}
-              </th>
-              <td className="px-6 py-4">{item.Email}</td>
-              <td className="px-6 py-4">{item.Location}</td>
-              <td className="px-6 py-4">{item.DateCreated}</td>
-              <td className="px-6 py-4">
-                <a
-                  href="/"
-                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+          {data.data !== null
+            ? data.data.map((item, index) => (
+                <tr
+                  key={index}
+                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
                 >
-                  Edit
-                </a>
-              </td>
-              <td className="px-6 py-4">
-                <a
-                  href="/"
-                  className="font-medium text-red-600 hover:underline"
-                  onClick={() => onDeleteHotel(item.Id)}
-                >
-                  Delete
-                </a>
-              </td>
-            </tr>
-          ))}
+                  <td className="w-4 p-4">
+                    <div className="flex items-center">
+                      <input
+                        id="checkbox-table-search-1"
+                        type="checkbox"
+                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                      />
+                      <label
+                        htmlFor="checkbox-table-search-1"
+                        className="sr-only"
+                      >
+                        checkbox
+                      </label>
+                    </div>
+                  </td>
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    {item.Name}
+                  </th>
+                  <td className="px-6 py-4">{item.Email}</td>
+                  <td className="px-6 py-4">{item.Location}</td>
+                  <td className="px-6 py-4">{item.DateCreated}</td>
+                  <td className="px-6 py-4">
+                    <a
+                      href="/"
+                      className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                    >
+                      Edit
+                    </a>
+                  </td>
+                  <td className="px-6 py-4">
+                    <a
+                      href="/"
+                      className="font-medium text-red-600 hover:underline"
+                      onClick={() => onDeleteHotel(item.Id)}
+                    >
+                      Delete
+                    </a>
+                  </td>
+                </tr>
+              ))
+            : ""}
         </tbody>
       </table>
     </div>
